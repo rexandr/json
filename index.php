d072e430ce0fadea47090078b6fd88860815a84f
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-05 00:32:00 +0200

    User and his role

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 function userContacts()
 {
-    if (file_exists('test.json')){
+    if (file_exists('test.json')) {
         $json = file_get_contents('test.json');
         $jsonArray = json_decode($json, true);
     }
-    
+
     echo '<pre>';
     print_r($jsonArray);
     echo '</pre>';
 
-    foreach ($jsonArray['users'] as $user)
-    {
-        echo $user['name'].'<br>';
-    }
+    foreach ($jsonArray['users'] as $user) {
+        $key = array_search($user['role'], array_column($jsonArray['roles'], 'id'));
+        echo $user['name']. $jsonArray['roles'][$key]['roleName'].'<br>';
 
+    }
 }
 
 userContacts();
