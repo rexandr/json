afa15e215a0247aeb1365b058b0ffcacecd1198f
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-05 15:19:42 +0200

    User's permissions added

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ function userContacts()
     foreach ($jsonArray['users'] as $user) {
         $role = array_search($user['role'], array_column($jsonArray['roles'], 'id'));
         $contact = array_search($user['id'], array_column($jsonArray['contacts'], 'user'));  //repeating check
+        $permission = array_search($user['permissions'], array_column($jsonArray['permissions'], 'id'));  //repeating check
 
         echo $user['name']. $jsonArray['roles'][$role]['roleName'].$jsonArray['contacts'][$role]['firstName'].
-            $jsonArray['contacts'][$role]['lastName'].'<br>';
-
+            $jsonArray['contacts'][$role]['lastName'].$jsonArray['permissions'][$permission]['value'].'<br>';
     }
 }
 
