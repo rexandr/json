f4125f471a80028c0aded8aa09a235a91cd8c9ae
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-06 00:20:06 +0200

    Roles can be added to the list

--- index.php
+++ index.php
@@ -1,9 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<a href="/">Home</a>
+<h1>Roles</h1>
+<?php foreach (rolesRead() as $values) { ?>
+    <?= $values['roleName'] ?><br>
+<?php } ?>
+
+<form action="" method="post">
+    <h3>Add role</h3>
+    <input type="text" name="role">
+    <input type="submit" value="Add"><br><br>
+</form>
+</body>
+</html>
+
 <?php
 function userContacts()
 {
     if (file_exists('test.json')) {
         $json = file_get_contents('test.json');
         $jsonArray = json_decode($json, true);
+    }
+
+    if (isset($_POST['role']))
+    {
+        $id = count($jsonArray['roles'])+1;
+        $jsonArray['roles'][]=['id'=>$id, 'roleName'=>$_POST['role']];
+        file_put_contents('test.json', json_encode($jsonArray));
+        unset($_POST);
     }
 
     foreach ($jsonArray['contacts'] as $contact) {
@@ -13,6 +45,17 @@ function userContacts()
         echo $jsonArray['users'][$user]['name'] . ', ' . $contact['firstName'] . ' ' . $contact['lastName'] . ', ' .
             $jsonArray['roles'][$role]['roleName'] . ' - ' . $jsonArray['permissions'][$permission]['value']. '<br>';
     }
+}
+
+function rolesRead()
+{
+    if (file_exists('test.json')) {
+        $json = file_get_contents('test.json');
+        $jsonArray = json_decode($json, true);
+        $roles=$jsonArray['roles'];
+    }
+
+    return $roles;
 }
 
 userContacts();
