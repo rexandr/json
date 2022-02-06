566f8872997805faa875f5b4b34ea99ca86248cb
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-06 18:31:09 +0200

    User can be added to the list

--- index.php
+++ index.php
@@ -27,15 +27,48 @@
 <?php } ?>
 
 <form action="" method="post">
-    <h3>Add role</h3>
+    <h3>Add permission</h3>
     <input type="text" name="permission">
     <input type="submit" value="Add"><br><br>
 </form>
+
+<h1>Users</h1>
+<?php foreach (usersRead() as $values) { ?>
+    Name  - <?= $values['name'] ?><br>
+    Role - <?= $values['role'] ?><br>
+    Permission - <?= $values['permissions'] ?><br>
+    <hr>
+<?php } ?>
+
+<form action="" method="post">
+    <h3>Add user</h3>
+    <div>
+        <h6>User's permission</h6>
+        <?php foreach (permissionsRead() as $values) { ?>
+            <input type="radio" id="permission" name="permissions" value="<?= $values['id'] ?>">
+            <label for="permission"><?= $values['value'] ?></label>
+            <br>
+        <?php } ?>
+    </div>
+
+    <div>
+        <h6>User's role</h6>
+        <?php foreach (rolesRead() as $values) { ?>
+            <input type="radio" id="role" name="roles" value="<?= $values['id'] ?>">
+            <label for="role"><?= $values['roleName'] ?></label>
+            <br>
+        <?php } ?>
+    </div>
+
+    <input type="text" name="users">
+    <input type="submit" value="Add"><br><br>
+</form>
+
 </body>
 </html>
 
-<?php
 
+<?php
 userContacts();
 
 function userContacts()
@@ -45,18 +78,24 @@ function userContacts()
         $jsonArray = json_decode($json, true);
     }
 
-    if (isset($_POST['role']))
-    {
-        $id = count($jsonArray['roles'])+1;
-        $jsonArray['roles'][]=['id'=>$id, 'roleName'=>$_POST['role']];
+    if (isset($_POST['role'])) {
+        $id = count($jsonArray['roles']) + 1;
+        $jsonArray['roles'][] = ['id' => $id, 'roleName' => $_POST['role']];
         file_put_contents('test.json', json_encode($jsonArray));
         unset($_POST);
     }
 
-    if (isset($_POST['permission']))
-    {
-        $id = count($jsonArray['permissions'])+1;
-        $jsonArray['permissions'][]=['id'=>$id, 'value'=>$_POST['permission']];
+    if (isset($_POST['permission'])) {
+        $id = count($jsonArray['permissions']) + 1;
+        $jsonArray['permissions'][] = ['id' => $id, 'value' => $_POST['permission']];
+        file_put_contents('test.json', json_encode($jsonArray));
+        unset($_POST);
+    }
+
+    if (isset($_POST['users'])) {
+        
+        $id = count($jsonArray['users']) + 1;
+        $jsonArray['users'][] = ['id' => $id, 'name' => $_POST['users'], 'role' => $_POST['roles'], 'permissions' => $_POST['permissions']];
         file_put_contents('test.json', json_encode($jsonArray));
         unset($_POST);
     }
@@ -66,7 +105,7 @@ function userContacts()
         $role = array_search($jsonArray['users'][$user]['role'], array_column($jsonArray['roles'], 'id'));
         $permission = array_search($jsonArray['users'][$user]['permissions'], array_column($jsonArray['permissions'], 'id'));
         echo $jsonArray['users'][$user]['name'] . ', ' . $contact['firstName'] . ' ' . $contact['lastName'] . ', ' .
-            $jsonArray['roles'][$role]['roleName'] . ' - ' . $jsonArray['permissions'][$permission]['value']. '<br>';
+            $jsonArray['roles'][$role]['roleName'] . ' - ' . $jsonArray['permissions'][$permission]['value'] . '<br>';
     }
 }
 
@@ -75,9 +114,9 @@ function rolesRead()
     if (file_exists('test.json')) {
         $json = file_get_contents('test.json');
         $jsonArray = json_decode($json, true);
-        $roles=$jsonArray['roles'];
+        $roles = $jsonArray['roles'];
     }
-    $roles=$jsonArray['roles'];
+    $roles = $jsonArray['roles'];
 
     return $roles;
 }
@@ -87,9 +126,20 @@ function permissionsRead()
     if (file_exists('test.json')) {
         $json = file_get_contents('test.json');
         $jsonArray = json_decode($json, true);
-        $permissions=$jsonArray['permissions'];
+        $permissions = $jsonArray['permissions'];
     }
 
     return $permissions;
+}
+
+function usersRead()
+{
+    if (file_exists('test.json')) {
+        $json = file_get_contents('test.json');
+        $jsonArray = json_decode($json, true);
+        $users = $jsonArray['users'];
+    }
+
+    return $users;
 }
 
