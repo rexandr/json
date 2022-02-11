ab7b8b9d0ed73ff6891c6196dafeb50d4bd263c0
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-11 23:09:16 +0200

    Rework 2

--- index.php
+++ index.php
@@ -4,72 +4,115 @@ function rate()
     if (file_exists('test.json')) {
         $json = file_get_contents('test.json');
         $jsonArray = json_decode($json, true);
+
+        foreach ($jsonArray as $arr) {
+            foreach ($arr as $key => $value) {
+                if ($key === 'from') {
+                    $fromSentence [] = mb_substr($value, 3, 2);
+                }
+                if ($key === 'to') {
+                    $toSentence [] = mb_substr($value, 3, 2);
+                }
+            }
+        }
+
+        $month = month((int)mb_substr($value, 0, 2));
+
+        $buffer = buffer($fromSentence, $toSentence);
+
+        echo arrayToString($month, $buffer);
+    }else{
+        echo "Choose a correct file!";
     }
+}
 
-    echo '<pre>';
-    print_r($jsonArray);
-    echo '</pre>';
+function month($number)
+{
+    switch ($number) {
+        case 1:
+            $month = 'Jan';
+            break;
+        case 2:
+            $month = 'Feb';
+            break;
+        case 3:
+            $month = 'Mar';
+            break;
+        case 4:
+            $month = 'Apr';
+            break;
+        case 5:
+            $month = 'May';
+            break;
+        case 6:
+            $month = 'Jun';
+            break;
+        case 7:
+            $month = 'Jul';
+            break;
+        case 8:
+            $month = 'Aug';
+            break;
+        case 9:
+            $month = 'Sep';
+            break;
+        case 10:
+            $month = 'Oct';
+            break;
+        case 11:
+            $month = 'Nov';
+            break;
+        case 12:
+            $month = 'Dec';
+            break;
+        default:
+            echo "Wrong month's title!";
+    }
+    return $month;
+}
 
-    $sentence = [];
+function arrayToString($month, $buffer)
+{
+    $string = $month.' ';
 
-    $buffer = 'Mar ';
-
-    foreach ($jsonArray as $arr) {
-        foreach ($arr as $key=>$value) {
-            if ($key === 'from')
-            {
-                $fromSentence [] = mb_substr($value, 3, 2);
-            }
-            if ($key === 'to')
-            {
-                $toSentence [] = mb_substr($value, 3, 2);
-            }
+    for ($j = 0; $j < count($buffer)-1; $j++) {
+        if ($j % 2 != 0) {
+            $string .= '-'.(int)$buffer[$j].', ';
+        }
+        if ($j % 2 == 0) {
+            $string .= (int)$buffer[$j];
         }
     }
 
-    echo '<pre>';
-    print_r($fromSentence);
-    echo '</pre>';
-    echo '<pre>';
-    print_r($toSentence);
-    echo '</pre>';
+    $string .= '-'.$buffer[count($buffer)-1];
 
-    $buffer=[];
+    return $string;
+}
 
+function buffer($fromSentence, $toSentence)
+{
     $buffer[] = $fromSentence[0];
 
-    $countArr = count($fromSentence)-1;
+    $countArr = count($fromSentence) - 1;
 
-
-    for ($i = 0; $i<$countArr; $i++)
-    {
-
-        if ($fromSentence[$i+1]<=$toSentence[$i] || $fromSentence[$i+1] - $toSentence[$i] < 2)
-        {
+    for ($i = 0; $i < $countArr; $i++) {
+        if ($fromSentence[$i + 1] <= $toSentence[$i] || $fromSentence[$i + 1] - $toSentence[$i] < 2) {
             continue;
         }
 
-        if ($fromSentence[$i]<$toSentence[$i])
-        {
+        if ($fromSentence[$i] < $toSentence[$i]) {
             $buffer[] = $toSentence[$i];
-            $buffer[] = $fromSentence[$i+1];
+            $buffer[] = $fromSentence[$i + 1];
         }
 
-        if ($fromSentence[$i+1]>$fromSentence[$i+2] && !empty($fromSentence[$i+2]))
-        {
-            $buffer[count($buffer)-1] = $fromSentence[$i+2];
+        if ($fromSentence[$i + 1] > $fromSentence[$i + 2] && !empty($fromSentence[$i + 2])) {
+            $buffer[count($buffer) - 1] = $fromSentence[$i + 2];
         }
-
     }
-
     $buffer[] = $toSentence[$countArr];
 
-    echo '<pre>';
-    print_r($buffer);
-    echo '</pre>';
-
+    return $buffer;
 }
 
 rate();
-
 ?>
