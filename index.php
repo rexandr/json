7a1dac61d53faf1dd21895953088592fa3351a30
Vjacheslav Golovach <syava_trader@ukr.net>
2022-02-13 19:04:08 +0200

    Final

--- index.php
+++ index.php
@@ -5,16 +5,6 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        #line_block {
-            width: 150px;
-            height: 50px;
-            float: left;
-            margin: 0 15px 15px 0;
-            text-align: left;
-            padding: 10px;
-        }
-    </style>
     <title>json</title>
 </head>
 <body>
